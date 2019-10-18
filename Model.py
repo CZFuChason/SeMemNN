a030249a0ba7d40d7ae415ea43ae1d710cffc8d2
CZFuChason <42126039+CZFuChason@users.noreply.github.com>
2019-10-18 18:06:22 +0900

    Add files via upload

---
+++ Model.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+from keras.models import Sequential, Model, load_model
+from keras.layers.embeddings import Embedding
+from keras.layers import Input, Activation, Dense, Permute, Dropout, Flatten
+from keras.layers import add, dot, concatenate
+from keras.layers import LSTM, Bidirectional
+from keras.utils.data_utils import get_file
+from keras.preprocessing.sequence import pad_sequences
+import numpy as np
+from keras.callbacks import EarlyStopping
+from keras import backend as K
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from keras_self_attention import SeqSelfAttention
+import os
+
+
+# In[2]:
+
+
+# Model
+def EMNN(max_sentence_length,len_vocab,num_classes, Mem_size, Dropout_rate=0.3, data_type='float16'):
+    # placeholders
+    des = Input((max_sentence_length,), dtype=data_type)
+    title = Input((max_sentence_length,), dtype=data_type)
+
+    # encoders
+    # embed the des sequence into a sequence of vectors
+    input_encoder_m = Sequential()
+    input_encoder_m.add(Embedding(input_dim=len_vocab,
+                                  output_dim=Mem_size,
+    #                               embeddings_initializer=my_init
+                                 ))
+    input_encoder_m.add(Dropout(Dropout_rate))
+    # output: (samples, max_len_tra_des_sent, embedding_dim)
+
+    input_encoder_c = Sequential()
+    input_encoder_c.add(Embedding(input_dim=len_vocab,
+                                  output_dim=max_sentence_length,
+    #                              embeddings_initializer=my_init
+                                 ))
+    input_encoder_c.add(Dropout(Dropout_rate))
+    # output: (samples, max_len_tra_des_sent, max_len_tra_title_sent)
+
+    # embed the title into a sequence of vectors
+    title_encoder = Sequential()
+    title_encoder.add(Embedding(input_dim=len_vocab,
+                                   output_dim=Mem_size,
+                                   input_length=max_sentence_length,
+    #                             embeddings_initializer=my_init
+                               ))
+    title_encoder.add(Dropout(Dropout_rate))
+    # output: (samples, max_len_tra_title_sent, embedding_dim)
+
+
+    input_encoded_m = input_encoder_m(des)
+    input_encoded_c = input_encoder_c(title)
+    title_encoded = title_encoder(title)
+
+
+    # shape: (samples, max_len_tra_des_sent, max_len_tra_title_sent)
+    match = dot([input_encoded_m, title_encoded], axes=(2, 2))
+    match = Activation('softmax')(match)
+
+    response = add([match, input_encoded_c])  # (samples, max_len_tra_des_sent, max_len_tra_title_sent)
+    response = Permute((2, 1))(response)  # (samples, max_len_tra_title_sent, max_len_tra_des_sent)
+
+    R = concatenate([response, title_encoded])
+    R = Bidirectional(LSTM(256,return_sequences=True,dropout=Dropout_rate))(R)
+    R = SeqSelfAttention(attention_activation='sigmoid')(R)
+#     R = Bidirectional(LSTM(64,dropout=Dropout_rate))(R)
+#     R = SeqSelfAttention(attention_activation='sigmoid')(R)
+    R = Flatten()(R)
+    result = Dense(num_classes)(R)  # (samples, vocab_size)
+    result = Activation('softmax')(result)
+    model = Model([des, title], result)
+    model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    model.summary()
+    return model
+
+
+# In[ ]:
+
+
+
+
---
+++ dataprocessing.py
@@ -0,0 +1,115 @@
+#Author Fu
+#Project Experience_Text_Classification
+
+
+import numpy as np
+from fastNLP import DataSet
+from fastNLP import Instance
+from fastNLP import Vocabulary
+
+
+def split_sent(instance):
+    return instance['description'].split()
+
+def token(dataset,max_sentence_length=None,vocab=None):
+    def padding_words(data):
+        for i in range(len(data)):
+            if data[i]['description_seq_len'] <= max_sentence_length:
+                padding = [-1] * (max_sentence_length - data[i]['description_seq_len'])
+                data[i]['description_words'] += padding
+#                 for _ in range(max_sentence_length - data[i]['description_seq_len']):
+#                     data[i]['description_words'].append(-1)
+            else:
+                pass
+        for i in range(len(data)):
+            if data[i]['title_seq_len'] <= max_sentence_length:
+                padding = [-1] * (max_sentence_length - data[i]['title_seq_len'])
+                data[i]['title_words'] += padding
+#                 for _ in range(max_sentence_length - data[i]['title_seq_len']):
+#                     data[i]['title_seq_len'].append(-1)
+            else:
+                pass
+        return data
+
+    max_des_len_train=0
+    max_title_len_train=0
+
+    for i in range (len(dataset)):
+        if(dataset[i]['description_seq_len'] > max_des_len_train):
+            max_des_len_train = dataset[i]['description_seq_len']
+        else:
+            pass
+    for i in range (len(dataset)):
+        if(dataset[i]['title_seq_len'] > max_title_len_train):
+            max_title_len_train = dataset[i]['title_seq_len']
+        else:
+            pass
+    if max_sentence_length==None:
+        max_sentence_length = max_des_len_train
+        if (max_title_len_train > max_sentence_length):
+            max_sentence_length = max_des_len_train
+    print ('max_sentence_length:',max_sentence_length)
+    
+    if vocab==None:
+        print('New Vocab')
+        vocab = Vocabulary(min_freq=2)
+        dataset.apply(lambda x:[vocab.add(word) for word in x['description_words']])
+        dataset.apply(lambda x:[vocab.add(word) for word in x['title_words']])
+        vocab.build_vocab()
+        
+        dataset.apply(lambda x: [vocab.to_index(word) for word in x['description_words']],
+                      new_field_name='description_words')
+        dataset.apply(lambda x: [vocab.to_index(word) for word in x['title_words']],
+                      new_field_name='title_words')
+        dataset= padding_words(dataset)
+    else:
+        print('Vocab exist')
+        dataset.apply(lambda x: [vocab.to_index(word) for word in x['description_words']],
+                      new_field_name='description_words')
+        dataset.apply(lambda x: [vocab.to_index(word) for word in x['title_words']],
+                      new_field_name='title_words')
+        dataset= padding_words(dataset)
+        
+    return dataset,len(vocab),max_sentence_length,vocab
+
+
+def load_file(file_path, max_len,vocab):
+    # read csv data to DataSet
+    dataset = DataSet.read_csv(file_path,headers=('label','title','description'),sep='","')
+    # preprocess data
+    dataset.apply(lambda x: int(x['label'][1])-1,new_field_name='label')
+#     dataset.apply(lambda x: int(x['label'][0])-1,new_field_name='label')
+    dataset.apply(lambda x: x['title'].lower(), new_field_name='title')
+    dataset.apply(lambda x: x['description'][:-2].lower()+' .', new_field_name='description')
+    dataset.apply(split_sent,new_field_name='description_words')
+    dataset.apply(lambda x: len(x['description_words']),new_field_name='description_seq_len')
+    dataset.apply(split_sent,new_field_name='title_words')
+    dataset.apply(lambda x: len(x['title_words']),new_field_name='title_seq_len')
+    dataset,len_vocab,max_sentence_length,vocab = token(dataset,max_len,vocab)
+    return dataset,len_vocab,max_sentence_length,vocab
+
+
+def tolist(dataset,shuffle=True):
+    tra_data_list=[]
+    for data in dataset:
+        tra_data_list.append([data['label'],data['title_words'],data['description_words']])
+    if shuffle:
+        np.random.shuffle(tra_data_list)
+    return tra_data_list
+
+
+def data_split(dataset,shuffle=True):
+    dataset=tolist(dataset,shuffle)
+    label, title, description = [], [], []
+    for data in dataset:
+        label.append(data[0])
+        title.append(data[1])
+        description.append(data[2])
+    return [label,title,description]
+
+
+def load_dataset(path,max_len=None,shuffle=True,vocab=None):
+    dataset,len_vocab,max_sentence_length, vocab = load_file(path,max_len,vocab)
+    [label,title,description]=data_split(dataset,shuffle=True)
+    return label,title,description,len_vocab,max_sentence_length,vocab
+
